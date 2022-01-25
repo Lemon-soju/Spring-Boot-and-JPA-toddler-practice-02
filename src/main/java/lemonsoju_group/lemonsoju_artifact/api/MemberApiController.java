0562fe27c57442c2885ac98b9732b9159668885b
Lemon Soju <yckk980320@naver.com>
2022-01-25 12:52:12 +0900

    User Update API

--- src/main/java/lemonsoju_group/lemonsoju_artifact/api/MemberApiController.java
+++ src/main/java/lemonsoju_group/lemonsoju_artifact/api/MemberApiController.java
@@ -2,11 +2,10 @@ package lemonsoju_group.lemonsoju_artifact.api;
 
 import lemonsoju_group.lemonsoju_artifact.domain.Member;
 import lemonsoju_group.lemonsoju_artifact.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -37,12 +36,35 @@ public class MemberApiController {
      */
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid
-                                                     CreateMemberRequest request) {
+                                                         CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.getName());
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request){
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id); //  커맨드와 쿼리는 분리하자
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+
 
     @Data
     static class CreateMemberRequest {
