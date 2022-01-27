534b4b79b999002478b4b15b63dcfc1ac569c3c4
Lemon Soju <yckk980320@naver.com>
2022-01-27 22:21:10 +0900

    ManyToOne Directly Inquire From JPA To DTO_Collection Inquire Optimization

--- src/main/java/lemonsoju_group/lemonsoju_artifact/api/OrderApiController.java
+++ src/main/java/lemonsoju_group/lemonsoju_artifact/api/OrderApiController.java
@@ -77,6 +77,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() { // F2 -> 다음 에러로 가기
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Data
     static class OrderDto{
 
